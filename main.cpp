5dd1040e1df2d5a6ae7505e2c4185196cf294844
Dictator <aouvarov8@gmail.com>
2021-02-03 16:53:38 +0200

    yy

--- main.cpp
+++ main.cpp
@@ -4,26 +4,31 @@ using namespace std;
 
 class Point {
 	private:
-		int x;
-		int y;
+		int _x;
+		int _y;
 	public:
 		Point() {};
-		Point(int x, int y) { this->x = x; this->y = y; }
-		void setX(int x) { this->x = x; };
-		void setY(int y) { this->y = y; };
-		int getX() { return x; };
-		int getY() { return y; };
+		Point(int x, int y) { this->_x = x; this->_y = y; }
+		~Point() { cout << "This is destructor call" << endl; }
+		void setX(int x) { this->_x = x; };
+		void setY(int y) { this->_y = y; };
+		int getX() { return _x; };
+		int getY() { return _y; };
 		void print();
 };
 
 void Point::print() {
-	cout << "Point: (" << x << ", " << y << ")" << endl;
+	cout << "Point: (" << _x << ", " << _y << ")" << endl;
 }
 
 
 int main() {
 	Point a(1, 2);
+	Point b;
+	Point* c = new Point(3, 5);
+	c->print();
 	a.print();
+	delete(c);
 	a.setX(10);
 	a.setY(20);
 	a.print();
